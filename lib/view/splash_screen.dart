9964dfdfb9bad40b298ba3613f8bedd935e8c7fc
Samsrtsns <sametkati30@gmail.com>
2024-10-17 14:53:31 +0300

    Splash Screen Done

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:newnewsapp/view/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/splash_pic.jpg',
+              fit: BoxFit.cover,
+              width: width,
+              height: height * .6,
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Text(
+              'TOP HEADLINES',
+              style: GoogleFonts.anton(
+                  letterSpacing: 6, color: Colors.grey.shade700, fontSize: 22),
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            SpinKitChasingDots(
+              color: Colors.orange.shade800,
+              size: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
